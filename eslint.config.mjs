730f12d1989f253e0762b719ed2248397d9bbc2b
Durk Steed Jr <durksteedjr@proton.me>
2025-07-17 17:50:41 -0400

    chore: configure prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import tseslint from "typescript-eslint";
 
 const config = tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  eslintConfigPrettier,
 );
 
 export default config;
